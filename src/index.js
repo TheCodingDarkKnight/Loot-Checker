1e91dcb499a1484cfb42aafa64fcb037c69e6c47
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-07-15 20:19:09 +0530

    Add App structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./components/App"
+import balance from "./reducers/balance";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(combineReducers({ balance }))
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"))
